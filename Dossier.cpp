d12e728e6eb86667069d80da4b0bc46969ea58dc
Jimmy Visse <jimmy.visse@gmail.com>
2014-05-28 18:32:31 +0200

    Nouvelle classe pour l'édition d'un dossier

--- Dossier.cpp
+++ Dossier.cpp
@@ -35,3 +35,38 @@ NewDossier::NewDossier(QWidget* parent)
     couche->addLayout(coucheH5);
     setLayout(couche);
 }
+
+EditDossier::EditDossier(Dossier* dToEdit, QWidget *parent): d(dToEdit), QWidget(parent) {
+    texte = new QLabel("Edition du dossier", this);
+
+    nomLabel = new QLabel("Nom: ", this);
+    prenomLabel = new QLabel("Prénom: ", this);
+    cursusLabel = new QLabel("Cursus: ", this);
+
+    nom = new QLineEdit(d->getNom(), this);
+    prenom = new QLineEdit(d->getPrenom(), this);
+    cursus = new QLineEdit(d->getCursus().getNom(), this);
+
+    bouton =  new QPushButton("Valider", this);
+
+    coucheH1 = new QHBoxLayout;
+    coucheH1->addWidget(texte);
+    coucheH2 = new QHBoxLayout;
+    coucheH2->addWidget(nomLabel);
+    coucheH2->addWidget(nom);
+    coucheH3 = new QHBoxLayout;
+    coucheH3->addWidget(prenomLabel);
+    coucheH3->addWidget(prenom);
+    coucheH4 = new QHBoxLayout;
+    coucheH4->addWidget(cursusLabel);
+    coucheH4->addWidget(cursus);
+    coucheH5 = new QHBoxLayout;
+    coucheH5->addWidget(bouton);
+    couche = new QVBoxLayout;
+    couche->addLayout(coucheH1);
+    couche->addLayout(coucheH2);
+    couche->addLayout(coucheH3);
+    couche->addLayout(coucheH4);
+    couche->addLayout(coucheH5);
+    setLayout(couche);
+}
--- Dossier.h
+++ Dossier.h
@@ -8,6 +8,7 @@
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
+#include "UTProfiler.h"
 
 class NewDossier: public QWidget {
     Q_OBJECT
@@ -31,5 +32,26 @@ public:
     explicit NewDossier(QWidget* parent=0);
 };
 
+class EditDossier: public QWidget {
+    Q_OBJECT
+private:
+    Dossier *d;
+    QLabel* texte;
+    QLabel* nomLabel;
+    QLabel* prenomLabel;
+    QLabel* cursusLabel;
+    QLineEdit* nom;
+    QLineEdit* prenom;
+    QLineEdit* cursus;
+    QPushButton* bouton;
+    QHBoxLayout* coucheH1;
+    QHBoxLayout* coucheH2;
+    QHBoxLayout* coucheH3;
+    QHBoxLayout* coucheH4;
+    QHBoxLayout* coucheH5;
+    QVBoxLayout* couche;
+public:
+    explicit EditDossier(Dossier *dToEdit, QWidget* parent=0);
+};
 
 #endif // DOSSIER_H
