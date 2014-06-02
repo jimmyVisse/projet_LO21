a5664dc076a106c68b135f983438e66d67c39e2c
Jimmy Visse <jimmy.visse@gmail.com>
2014-06-02 21:08:51 +0200

    Affichage du nombre de crédits acquis pour un dossier

--- Dossier.cpp
+++ Dossier.cpp
@@ -40,11 +40,82 @@ NewDossier::NewDossier(QWidget *parent): DossierEdit(parent)
     setLayout(couche);
 }
 
-EditDossier::EditDossier(Dossier* dToEdit, QWidget *parent): d(dToEdit), DossierEdit(parent) {
+EditDossier::EditDossier(Dossier& dToEdit, QWidget *parent): d(dToEdit), DossierEdit(parent) {
     texte->setText("Edition du dossier");
 
-    nom->setText(d->getNom());
-    prenom->setText(d->getPrenom());
-    cursus->setText(d->getCursus().getNom());
+    ajouter = new QPushButton("Ajouter inscription", this);
+
+    cred = new QLabel("Nombre de crédits: ", this);
+    credits = new QSpinBox(this);
+    credits->setValue(d.getNbCredits());
+
+    nom->setText(d.getNom());
+    prenom->setText(d.getPrenom());
+    cursus->setText(d.getCursus().getNom());
+    cursus->setReadOnly(true);
+
+    coucheH4->addWidget(cred);
+    coucheH4->addWidget(credits);
+    coucheH5->addWidget(ajouter);
     setLayout(couche);
+
+    connect(bouton, SIGNAL(clicked()), this, SLOT(modifDossier()));
+    connect(ajouter, SIGNAL(clicked()), this, SLOT(ajoutIns()));
+}
+
+void EditDossier::modifDossier()
+{
+    Dossier::getInstance().setNom(nom->text());
+    Dossier::getInstance().setPrenom(prenom->text());
+    Dossier::getInstance().setCursus(Formation(cursus->text()));
+    QMessageBox::information(this, "Edition d'un dossier", "Changements effectués");
+}
+
+void EditDossier::ajoutIns()
+{
+    AjoutDossier* fen = new AjoutDossier(this);
+    fen->exec();
+}
+
+AjoutDossier::AjoutDossier(QWidget *parent): QDialog(parent)
+{
+    uvLabel = new QLabel("UV: ", this);
+    noteLabel = new QLabel("Note: ", this);
+    semLabel = new QLabel("Semestre: ", this);
+
+    uv = new QLineEdit(this);
+    note = new QLineEdit(this);
+    semestre = new QLineEdit(this);
+
+    valider = new QPushButton("Valider", this);
+
+    coucheH1 = new QHBoxLayout;
+    coucheH1->addWidget(uvLabel);
+    coucheH1->addWidget(uv);
+    coucheH2 = new QHBoxLayout;
+    coucheH2->addWidget(noteLabel);
+    coucheH2->addWidget(note);
+    coucheH3 = new QHBoxLayout;
+    coucheH3->addWidget(semLabel);
+    coucheH3->addWidget(semestre);
+    coucheH4 = new QHBoxLayout;
+    coucheH4->addWidget(valider);
+    couche = new QVBoxLayout;
+    couche->addLayout(coucheH1);
+    couche->addLayout(coucheH2);
+    couche->addLayout(coucheH3);
+    couche->addLayout(coucheH4);
+    setLayout(couche);
+
+    connect(valider, SIGNAL(clicked()), this, SLOT(ajouter()));
+}
+
+void AjoutDossier::ajouter()
+{
+    try {
+        Dossier::getInstance().ajoutInscription(uv->text(), note->text(), semestre->text());
+        QMessageBox::information(this, "Ajouter une inscription", "Inscription ajoutée");
+    }catch(UTProfilerException& e) {
+        QMessageBox::warning(this, "Ajouter une inscription", "Erreur dans l'ajout de l'inscription: "+e.getInfo());
+    }
 }
--- Dossier.h
+++ Dossier.h
@@ -5,13 +5,15 @@
 #include <QWidget>
 #include <QLabel>
 #include <QLineEdit>
+#include <QSpinBox>
 #include <QPushButton>
 #include <QHBoxLayout>
 #include <QVBoxLayout>
+#include <QDialog>
+#include <QMessageBox>
 #include "UTProfiler.h"
 
 class DossierEdit: public QWidget {
-    Q_OBJECT
 protected:
     QLabel* texte;
     QLabel* nomLabel;
@@ -31,17 +33,46 @@ public:
     DossierEdit(QWidget* parent=0);
 };
 
-
 class NewDossier: public DossierEdit {
+    Q_OBJECT
 public:
     explicit NewDossier(QWidget *parent=0);
+public slots:
 };
 
 class EditDossier: public DossierEdit {
+    Q_OBJECT
 private:
-    Dossier* d;
+    Dossier& d;
+    QLabel* cred;
+    QPushButton* ajouter;
+    QSpinBox* credits;
 public:
-    explicit EditDossier(Dossier *dToEdit, QWidget* parent=0);
+    explicit EditDossier(Dossier &dToEdit, QWidget* parent=0);
+public slots:
+    void modifDossier();
+    void ajoutIns();
+};
+
+class AjoutDossier: public QDialog {
+    Q_OBJECT
+private:
+    QLabel* uvLabel;
+    QLabel* noteLabel;
+    QLabel* semLabel;
+    QLineEdit* uv;
+    QLineEdit* note;
+    QLineEdit* semestre;
+    QPushButton* valider;
+    QHBoxLayout* coucheH1;
+    QHBoxLayout* coucheH2;
+    QHBoxLayout* coucheH3;
+    QHBoxLayout* coucheH4;
+    QVBoxLayout* couche;
+public slots:
+    void ajouter();
+public:
+    explicit AjoutDossier(QWidget* parent=0);
 };
 
 #endif // DOSSIER_H
